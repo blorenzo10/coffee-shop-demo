9d97c9c80e9ad718f1079e1beb92f16e635cd7ec
Bruno Lorenzo <bruno.lorenzop@gmail.com>
2023-06-25 17:28:39 -0300

    Initial Commit

---
+++ automatic-grammar-agreement-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  automatic-grammar-agreement-demo
+//
+//  Created by Bruno Lorenzo on 25/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ automatic-grammar-agreement-demo/automatic_grammar_agreement_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  automatic_grammar_agreement_demoApp.swift
+//  automatic-grammar-agreement-demo
+//
+//  Created by Bruno Lorenzo on 25/6/23.
+//
+
+import SwiftUI
+
+@main
+struct automatic_grammar_agreement_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
